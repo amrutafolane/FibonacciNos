95a60f3e26eaae0b1cb03b6deedb93f80da846f8
Amruta Folane <amrutafolane@gmail.com>
2016-12-24 22:08:42 -0600

    Add files via upload

---
+++ FibonacciNos.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class FibonacciNos {
+
+	public int findFibo (int n) {
+
+		if (n == 0 || n == 1) {				 //base cases
+			return n;
+		}               
+
+		else {                      		  //recursive
+			int fib = (findFibo(n-1) + findFibo(n-2));
+			return fib;
+		}   
+	}  
+
+
+
+	public static void main (String []args) {
+
+		FibonacciNos fib = new FibonacciNos();
+
+		for (int i=0; i<=20; i++) {
+			System.out.println(fib.findFibo(i));
+
+		}
+	}
+
+}
